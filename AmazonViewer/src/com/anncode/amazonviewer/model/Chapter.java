faaa7a451fa6745b48794c89d068146adb28b317
Dieter Prado <dayterp@gmail.com>
2021-07-09 16:30:35 -0500

    Adding files

--- AmazonViewer/src/com/anncode/amazonviewer/model/Chapter.java
+++ AmazonViewer/src/com/anncode/amazonviewer/model/Chapter.java
@@ -3,8 +3,8 @@ package com.anncode.amazonviewer.model;
 import java.util.ArrayList;
 
 public class Chapter extends Movie {
-	
-	
+
+
 	private int id;
 	private int sessionNumber;
 	private Serie serie;
@@ -15,7 +15,7 @@ public class Chapter extends Movie {
 		this.setSessionNumber(sessionNumber);
 		this.setSerie(serie);
 	}
-	
+
 	@Override
 	public int getId() {
 		// TODO Auto-generated method stub
@@ -29,8 +29,8 @@ public class Chapter extends Movie {
 	public void setSessionNumber(int sessionNumber) {
 		this.sessionNumber = sessionNumber;
 	}
-	
-	
+
+
 	public Serie getSerie() {
 		return serie;
 	}
@@ -42,23 +42,40 @@ public class Chapter extends Movie {
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return  "\n :: SERIE ::" + 
+		return  "\n :: SERIE ::" +
 				"\n Title: " + getSerie().getTitle() +
-				"\n :: CHAPTER ::" + 
+				"\n :: CHAPTER ::" +
 				"\n Title: " + getTitle() +
-				"\n Year: " + getYear() + 
+				"\n Year: " + getYear() +
 				"\n Creator: " + getCreator() +
 				"\n Duration: " + getDuration();
 	}
-	
-	
+
+
 	public static ArrayList<Chapter> makeChaptersList(Serie serie) {
 		ArrayList<Chapter> chapters = new ArrayList();
-		
+
 		for (int i = 1; i <= 5; i++) {
 			chapters.add(new Chapter("Capituo "+i, "genero "+i, "creator" +i, 45, (short)(2017+i), i, serie));
 		}
-		
+
 		return chapters;
+	}
+
+	@Override
+	public void view() {
+		// TODO Auto-generated method stub
+		super.view();
+		ArrayList<Chapter> chapters =  getSerie().getChapters();
+		int chapterViewedCounter = 0;
+		for (Chapter chapter : chapters) {
+			if (chapter.getIsViewed()) {
+				chapterViewedCounter++;
+			}
+		}
+
+		if (chapterViewedCounter == chapters.size()) {
+			getSerie().view();
+		}
 	}
 }
